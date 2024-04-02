ac76101d07f1ad423b62161fd7f382da6d61ad99
Falconsc <75039653+Falcons1231@users.noreply.github.com>
2024-04-02 18:23:32 -0400

    Added Items Entity

---
+++ Online Shopping Final Project/Online Shopping Final Project/Data/ShoppingContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Shopping_Final_Project.Entities;
+
+namespace Online_Shopping_Final_Project.Data
+{
+    public class ShoppingContext:DbContext
+    {
+        public ShoppingContext(DbContextOptions<ShoppingContext> options) : base(options) { }
+
+        public DbSet<Order> OrderHistory { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
