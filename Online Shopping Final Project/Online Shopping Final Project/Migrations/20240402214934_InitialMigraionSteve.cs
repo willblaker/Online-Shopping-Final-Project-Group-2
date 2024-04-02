3cdbaf30bd7abf3a3bee7e0ab4f7a87a68377d8d
Falconsc <75039653+Falcons1231@users.noreply.github.com>
2024-04-02 18:40:48 -0400

    With Items table

---
+++ Online Shopping Final Project/Online Shopping Final Project/Migrations/20240402214934_InitialMigraionSteve.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShoppingFinalProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigraionSteve : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ItemDescription = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ItemPrice = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.ItemId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
