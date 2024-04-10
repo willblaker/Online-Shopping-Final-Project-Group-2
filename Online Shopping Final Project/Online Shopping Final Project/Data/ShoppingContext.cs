249c6623c1f269cb1f813ec16e3dacbeefbb20af
Logan Kaas <kaaslc@mail.uc.edu>
2024-04-10 18:16:53 -0400

    added services/controllers for CRUD events

--- Online Shopping Final Project/Online Shopping Final Project/Data/ShoppingContext.cs
+++ Online Shopping Final Project/Online Shopping Final Project/Data/ShoppingContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Online_Shopping_Final_Project.Client.Pages;
 using Online_Shopping_Final_Project.Entities;
 
 namespace Online_Shopping_Final_Project.Data
@@ -8,6 +9,18 @@ namespace Online_Shopping_Final_Project.Data
         public ShoppingContext(DbContextOptions<ShoppingContext> options) : base(options) { }
 
         public DbSet<Order> OrderHistory { get; set; }
+
+        // UNCOMMENT WHEN MIGRATIONS FOR THESE ARE BUILT
+
+        //public DbSet<M_Tshirts> tShirts { get; set; }
+
+        //public DbSet<M_Hoodies> hoodies { get; set; }
+
+        //public DbSet<M_Shorts> shorts { get; set; }
+
+        //public DbSet<M_Pants> pants { get; set; }
+
+        //public DbSet<M_Shoes> shoes { get; set; }
 
         public DbSet<Item> Items { get; set; }
     }
