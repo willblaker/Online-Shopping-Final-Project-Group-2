e54fa1b3c7a0d1d74e24e4fcbf03bbafb6df8e02
Nhi Nguyen <nhing17899@gmail.com>
2024-04-13 18:20:00 -0400

    Seeded table, New Razor component, and ViewModel for User

--- Online Shopping Final Project/Online Shopping Final Project/Data/ShoppingContext.cs
+++ Online Shopping Final Project/Online Shopping Final Project/Data/ShoppingContext.cs
@@ -25,5 +25,51 @@ namespace Online_Shopping_Final_Project.Data
         public DbSet<Item> Items { get; set; }
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasData(
+                new User 
+                { 
+                    UserId = 1,
+                    Username = "user 1", 
+                    Email = "user1@onlineshopping.com", 
+                    PasswordHash = "hashedpassword1", 
+                    FirstName = "User", 
+                    LastName = "User1", 
+                    Address = "123 User1 St", 
+                    City = "City1", 
+                    State = "State1", 
+                    PostalCode = "45219" 
+                },
+                new User
+                {
+                    UserId = 2,
+                    Username = "user 2",
+                    Email = "user2@onlineshopping.com",
+                    PasswordHash = "hashedpassword2",
+                    FirstName = "User",
+                    LastName = "User2",
+                    Address = "123 User2 St",
+                    City = "City2",
+                    State = "State2",
+                    PostalCode = "45219"
+                },
+                new User
+                {   
+                    UserId = 3,
+                    Username = "user 3",
+                    Email = "user3@onlineshopping.com",
+                    PasswordHash = "hashedpassword3",
+                    FirstName = "User",
+                    LastName = "User3",
+                    Address = "123 User3 St",
+                    City = "City3",
+                    State = "State3",
+                    PostalCode = "45219"
+                }
+            );
+        }
     }
 }
---
+++ Online Shopping Final Project/Online Shopping Final Project/Migrations/20240413220942_UserSeededTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OnlineShoppingFinalProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserSeededTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "UserId", "Address", "City", "Email", "FirstName", "LastName", "PasswordHash", "PostalCode", "State", "Username" },
+                values: new object[,]
+                {
+                    { 1, "123 User1 St", "City1", "user1@onlineshopping.com", "User", "User1", "hashedpassword1", "45219", "State1", "user 1" },
+                    { 2, "123 User2 St", "City2", "user2@onlineshopping.com", "User", "User2", "hashedpassword2", "45219", "State2", "user 2" },
+                    { 3, "123 User3 St", "City3", "user3@onlineshopping.com", "User", "User3", "hashedpassword3", "45219", "State3", "user 3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "UserId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "UserId",
+                keyValue: 3);
+        }
+    }
+}
