00161ccb111d005c3e63a97aa418228a999522a3
Nhi Nguyen <nhing17899@gmail.com>
2024-04-17 16:12:31 -0400

    Resolve conflict

--- Online Shopping Final Project/Online Shopping Final Project.Client/Program.cs
+++ Online Shopping Final Project/Online Shopping Final Project.Client/Program.cs
@@ -3,6 +3,6 @@ using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddSingleton<HttpClient>(new HttpClient { BaseAddress = new Uri("https://localhost:7126") });
+builder.Services.AddSingleton<HttpClient>(new HttpClient { BaseAddress = new Uri("https://localhost:7126/") });
 
 await builder.Build().RunAsync();
